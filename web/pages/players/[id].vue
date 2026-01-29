44b4327f6154f27f711d61dbef4cf46b43b86195
olixc <noibiabdulrasaq@rocketmail.com>
2026-01-29 22:56:07 +0100

    fix: remove up arrows from Save and Contact buttons

--- web/pages/players/[id].vue
+++ web/pages/players/[id].vue
@@ -353,7 +353,7 @@
             <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
-            Save ⬆
+            Save
           </button>
         </NuxtLink>
 
@@ -373,7 +373,7 @@
             <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             </svg>
-            Contact ⬆
+            Contact
           </button>
         </NuxtLink>
 
